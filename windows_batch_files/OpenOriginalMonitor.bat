41c1d56f9741917bbb3428b133088507d11c2573
adamsl <eg1972@gmail.com>
2022-02-21 09:22:22 -0500

    added wordpress shortcuts

---
+++ windows_batch_files/OpenOriginalMonitor.bat
@@ -0,0 +1,2 @@
+wsl.exe code /home/adamsl/monitor
+wsl.exe --cd /home/adamsl/monitor
---
+++ windows_batch_files/Wordpress.bat
@@ -0,0 +1,2 @@
+wsl.exe code /mnt/c/xampp-joomla/htdocs/wordpress/wp-content/plugins/MCBA-Wordpress
+wsl.exe --cd /mnt/c/xampp-joomla/htdocs/wordpress/wp-content/plugins/MCBA-Wordpress
