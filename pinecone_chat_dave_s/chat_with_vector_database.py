4a1c6cf509287b9dbee787cea5c09610c5bdc50f
egadams <eg1972@gmail.com>
2025-02-04 23:35:14 -0500

    fixing chat with lance wip

--- pinecone_chat_dave_s/chat_with_vector_database.py
+++ pinecone_chat_dave_s/chat_with_vector_database.py
@@ -100,31 +100,67 @@ if __name__ == '__main__':
         ###
         data_for_pinecone_upsert = list()  # initialize the list that will ultimately be upserted to the vector database
         if ( USE_RUN_TEXT == True ):
-            temp = input( "using run.txt, ok? <enter> to continue.  ctrl-c to quit" )
+            temp = input( "using run.txt, ok? <enter> to continue.  ctrl-c to quit" ) # check run.txt
             user_input = open_file( '/home/adamsl/linuxBash/agents/run.txt' )
             USE_RUN_TEXT = False
         else:
             user_input = input( '\n\nUSER: ' )
         timestamp = time()
         timestring = timestamp_to_datetime( timestamp )
-        embedded_user_input = gpt3_embedding( user_input )
+        embedded_user_input = gpt3_embedding( user_input ) # gpt3 embed this user input
         unique_id = str( uuid4())
         metadata = { 'speaker': 'USER', 'time': timestamp, 'message': user_input, 'timestring': timestring, 'uuid': unique_id }
         save_json( '/home/adamsl/linuxBash/agents/nexus/%s.json' % unique_id, metadata ) # <<--- save user input to a .json file on our file system ---<<<
-        data_for_pinecone_upsert.append(( unique_id, embedded_user_input ))  # <<--- this data is going to pinecone ---<<<
+        #
+        # so the interface is append( String, GPT3 embedding ) 
+        # something that takes a string and a gpt3 embedding, and returns nothing
+        #
+        # void append( String, GPT3 embedding )
+        
+        # took me a minute to realize that we do not need the PineconeInterface interface.  we are using the IVectorDatabase interface instead.
+        # class PineconeInterface:
+        #     def append( self, unique_id: str, embedding: list ) -> None:
+        #         pass        
+    
+        # that would be my first most useful interface.
+        # thanks for the help, dave.
+        # i have always thought about what it would be like to see a piece of code and think to myself, "I could use an interface here"
+        # I prayed to the Father about gaining understanding earler.  Thank you Father.
+        data_for_pinecone_upsert.append(( unique_id, embedded_user_input ))  # <<--- this data is eventually going to pinecone ---<<<
+
+        # why was the reading of the mcp23... chip so difficult?  Maybe because the interface was not recognized soon enough.  Next time, recognize where all of the interfaces are before we even start to write any code.
+
         ###
         ###  Now we have the user input not only saved to our local file, but it is also placed in the built-in mutable
         ###  sequence that we will ultimately be inserted into the vector database under the same unique_id.
         ###
-        results = vdb.query( vector=embedded_user_input, top_k=convo_length )# search for relevant message unique ids in vsd
+        class IVectorDatabase:
+            """
+            An interface for a vector database that provides methods for querying and upserting vectors.
+            
+            The `query` method takes a vector and the number of top results to return, and returns a dictionary of the top matching results.
+            The `upsert` method takes a list of vectors and inserts or updates them in the vector database.
+            """
+            def query(self, vector: list, top_k: int) -> dict:
+                pass
+            
+            def upsert(self, vectors: list) -> None:
+                pass     
+           
+        vector_database = IVectorDatabase() # this has not been completely converted to an Interface yet since its methods are implemented.
+        results = vector_database.query( vector=embedded_user_input, top_k=convo_length )  # search for relevant message unique ids in vsd        
         conversation = load_conversation( results )  # with these unique ids, which where very cheap to aquire, we load the
                                                      # relevant conversation data from our local file system
+
+        # this was the previous code which didn't work well since i want to use the free chroma vdb and not be tied down.
+        # results = vdb.query( vector=embedded_user_input, top_k=convo_length )# search for relevant message unique ids in vsd
+
         prompt = open_file( 'prompt_response.txt' ).replace( '<<CONVERSATION>>', conversation ).replace( '<<MESSAGE>>', user_input )
         ###
         ai_completion_text = ai_completion( prompt )  # <<-- send the prompt created from the template to the model ---<<<
         timestamp = time()
         timestring = timestamp_to_datetime( timestamp )
-        embedded_ai_completion = gpt3_embedding( ai_completion_text )
+        embedded_ai_completion = gpt3_embedding( ai_completion_text ) # GPT3 embed the ai completion text
         unique_id = str( uuid4())
         metadata = { 'speaker': 'RAVEN', 'time': timestamp, 'message': ai_completion_text,
                      'timestring': timestring, 'uuid': unique_id }
@@ -132,9 +168,8 @@ if __name__ == '__main__':
         save_json( '/home/adamsl/linuxBash/agents/nexus/%s.json' % unique_id, metadata ) # <<--- save ai answer to a .json file ---<<<
         ###
         data_for_pinecone_upsert.append(( unique_id, embedded_ai_completion )) # <<--- add ai answer to data to be upserted ---<<<
-        vdb.upsert( data_for_pinecone_upsert ) # <<--- upsert the data to pinecone ---<<<
-        print( '\n\nRAVEN: %s' % ai_completion_text )  
-        
+        vector_database.upsert( data_for_pinecone_upsert ) # <<--- upsert the data to pinecone ---<<<
+        print( '\n\nRAVEN: %s' % ai_completion_text )        
         # just noticed that the unique_id is being used in the upsert and
         # the save_json, so it's being saved twice. How are we referencing both?
         # we "tag" the data here on our file system.  the vector database
